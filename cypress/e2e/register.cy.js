0daa6c015199a4607eafc6fc7c1fc959a9185ad8
ozgecanodemis <odemisozgecan@gmail.com>
2024-09-06 17:46:30 +0300

    positive-tests-ok

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -55,57 +55,17 @@ describe('Register Page', () => {
   })
   describe("Form inputs validated", () => {
 
-    it('button enabled for validated inputs', () => {
+    it.only('button enabled for validated inputs', () => {
       //arrange
       cy.visit('http://localhost:5174/')
       // act 
       cy.get('[data-cy="ad-input"]').type("emre");
-      cy.get('[data-cy="soyad-input"]').type("şa");
-      cy.get('[data-cy="email-input"]').type("emre@wit.");
-      cy.get('[data-cy="password-input"]').type("1234");
+      cy.get('[data-cy="soyad-input"]').type("Şahiner");
+      cy.get('[data-cy="email-input"]').type("emre@wit.com.tr");
+      cy.get('[data-cy="password-input"]').type("1234**");
 
       //assert
-      cy.contains(errorMessages.ad);
+      cy.get('[data-cy="password-input"]').should("not.be.disabled");
     })
-
-    it('surname input throws error for 2 chars', () => {
-      //arrange
-      cy.visit('http://localhost:5174/')
-      // act 
-      cy.get('[data-cy="soyad-input"]').type("şa");
-      //assert
-      cy.contains(errorMessages.soyad);
-    })
-
-    it('email input throws error for emre@wit.', () => {
-      //arrange
-      cy.visit('http://localhost:5174/')
-      // act 
-      cy.get('[data-cy="email-input"]').type("emre@wit.");
-      //assert
-      cy.contains(errorMessages.email);
-
-    })
-
-    it('Password input throws error for 1234', () => {
-      //arrange
-      cy.visit('http://localhost:5174/')
-      // act 
-      cy.get('[data-cy="password-input"]').type("1234");
-      //assert
-      cy.contains(errorMessages.password);
-
-    })
-
-    it('button is disabled for unvalidated inputs', () => {
-      //arrange
-      cy.visit('http://localhost:5174/')
-      // act 
-      cy.get('[data-cy="password-input"]').type("1234");
-      //assert
-      cy.get('[data-cy="password-input"]').should("be.disabled");
-
-    })
-
   })
 })
